7b1460339b7da3358748ecc603ff70ee52e6a8c6
Vesa Rautiainen <veskurau@gmail.com>
2023-11-09 13:16:57 +0200

    Complete project reader

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,17 +1,19 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license_info, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license_info
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
     def _stringify_dependencies(self, dependencies):
-        return ", ".join(dependencies) if len(dependencies) > 0 else "-"
+        return "\n - ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
         return (
             f"Name: {self.name}"
             f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
+            f"\nLicense: {self.license or '-'}\n"
+            f"\nDependencies: \n - {self._stringify_dependencies(self.dependencies)}\n"
+            f"\nDevelopment dependencies: \n - {self._stringify_dependencies(self.dev_dependencies)}"
         )
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -10,22 +10,13 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        #print(content)
         parsed_toml = toml.loads(content)
-        #print(parsed_toml)
+
         name = parsed_toml["tool"]["poetry"]["name"]
         description = parsed_toml["tool"]["poetry"]["description"]
+        license_info = parsed_toml["tool"]["poetry"]["license"]
         dependencies = parsed_toml["tool"]["poetry"]["dependencies"]
         dev_dependencies = parsed_toml["tool"]["poetry"]["group"]["dev"]["dependencies"]
-        #rint(dependencies)
-        #print(f"Name on: {name}")
-
-        #new_toml_string = toml.dumps(parsed_toml)
-        #print(new_toml_string)
-
-        # with open('new_toml_file.toml', 'w') as f:
-        #     new_toml_string = toml.dump(parsed_toml, f)
-        # print(new_toml_string)
-
+        
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project(name, description, dependencies, dev_dependencies)
+        return Project(name, description, license_info, dependencies, dev_dependencies)

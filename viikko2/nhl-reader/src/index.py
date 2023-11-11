4b23c805f525476006f1e7f3d5f83cb3b7d9b0ed
Vesa Rautiainen <veskurau@gmail.com>
2023-11-11 10:37:30 +0200

    Complete task 10

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,23 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print("Oliot:")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
